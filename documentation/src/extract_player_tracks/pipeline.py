e271a9c14117b429dd7407810b29b2082cc3f6e1
leharris3 <levlevi@email.unc.edu>
2024-06-07 04:26:52 -0400

    docs

--- documentation/src/extract_player_tracks/pipeline.py
+++ documentation/src/extract_player_tracks/pipeline.py
@@ -1,5 +1,9 @@
-def extract_player_tracks_from_dir(dir_path: str, dst_path: str):
-    pass
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 def extract_player_tracks_from_video(video_path: str, dst_path: str):
+    pass
+
+
+def extract_player_tracks_from_dir(dir_path: str, dst_path: str):
     pass
