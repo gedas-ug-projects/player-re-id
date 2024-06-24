629e92ecc7ef036552171c61d9046b83acda737f
leharris3 <levlevi@email.unc.edu>
2024-06-24 01:17:42 -0400

    26/50 - florence

--- src/data/legend.md
+++
@@ -1,3 +0,0 @@
-### Bounding Box Files Legend
-- Example row: `1, 0, 102, 271, 43, 116, 1, 1, 1`
-- Keys: `frame, id, x1, y1, width, height, pad, pad, pad`
--- src/testing/embed_players/full_track_benchmark.py
+++ src/testing/embed_players/full_track_benchmark.py
@@ -1,18 +1,14 @@
-import cv2
-import shutil
 import json
 import os
 import logging
-import sys
 import torch.multiprocessing as mp
 import warnings
 
 from glob import glob
-from PIL import Image, ImageOps
+from PIL import Image
+from transformers import AutoProcessor, AutoModelForCausalLM 
 from tqdm import tqdm
 
-MODEL_NAME = 'openbmb/MiniCPM-Llama3-V-2_5'
-MINI_CPM_DIR = "/mnt/opr/levlevi/player-re-id/src/testing/mini_cpm_testing/mini_cpm/MiniCPM-V"
 PROMPT = """Analyze the basketball player shown in the provided still tracklet frame and describe the following details:
 1. Jersey Number: Identify the number on the player's jersey. If the player has no jersey, provide None.
 Based on the frame description, produce an output prediction in the following JSON format:
@@ -21,14 +17,6 @@ Based on the frame description, produce an output prediction in the following JS
 }
 [EOS]"""
 
-if os.path.exists(MINI_CPM_DIR):
-    sys.path.append(MINI_CPM_DIR)
-    os.chdir(MINI_CPM_DIR)
-else:
-    raise FileNotFoundError(f"Directory {MINI_CPM_DIR} does not exist")
-
-from chat import MiniCPMVChat, img2base64
-
 warnings.simplefilter(action='ignore', category=FutureWarning)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -36,61 +24,50 @@ logger = logging.getLogger(__name__)
 def load_model_and_tokenizer(device: int = 0):
     try:
         logger.info("Loading model and tokenizer...")
-        model = MiniCPMVChat(MODEL_NAME, device)
-        logger.info("Model and tokenizer loaded successfully.")
-        return model
+        model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-large-ft", trust_remote_code=True, device_map="cuda").to(device).eval()
+        processor = AutoProcessor.from_pretrained("microsoft/Florence-2-large-ft", trust_remote_code=True)
+        return model, processor
     except Exception as e:
         logger.error(f"Failed to load model or tokenizer: {e}")
         raise
 
-def ocr(image_base64, model):
-    try:
-        question = PROMPT
-        msgs = [{'role': 'user', 'content': question}]
-        inputs = {"image": image_base64, "question": json.dumps(msgs)}
-        answer = model.chat(inputs)
-        result = answer
-        return result
-    except Exception as e:
-        logger.error(f"Failed to perform OCR: {e}")
-        return ""
+def ocr(image_file_path: str, model, processor, device) -> str:
+    image = Image.open(image_file_path)
+    inputs = processor(text=PROMPT, images=image, return_tensors="pt").to(device)
+    generated_ids = model.generate(
+        input_ids=inputs["input_ids"],
+        pixel_values=inputs["pixel_values"],
+        max_new_tokens=1024,
+        do_sample=False,
+        num_beams=3
+    )
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
+    parsed_answer = processor.post_process_generation(generated_text, task="<OCR>", image_size=(image.width, image.height))
+    return parsed_answer
 
-def load_and_convert_image(fp: str):
-    try:
-        return img2base64(fp)
-    except Exception as e:
-        logger.error(f"Failed to load or convert image {fp}: {e}")
-        return None
-
-def process_image(image_fp: str, model):
-    image_base64 = load_and_convert_image(image_fp)
-    if image_base64:
-        result = ocr(image_base64, model)
-        return result
-    return None
-
-def ocr_dir(dir_fp: str, model):
+def ocr_dir(dir_fp: str, model, processor, deivce):
     img_paths = glob(os.path.join(dir_fp, '*.jpg'))
     results = {}
     for idx, img_path in enumerate(tqdm(img_paths, total=len(img_paths))):
-        result = process_image(img_path, model)
+        result = ocr(img_path, model, processor, deivce)
         if result:
             results[idx] = result
     logger.info(f"Results for directory {dir_fp}: {results}")
     return dir_fp, results
 
 def process_dir(dirs, device: int = 0, all_results=None):
-    model = load_model_and_tokenizer(device)
+    model, processor = load_model_and_tokenizer(device)
     for dir_fp in dirs:
         logger.info(f"Processing directory: {dir_fp}")
-        dir_fp, dir_result = ocr_dir(dir_fp, model)
+        dir_fp, dir_result = ocr_dir(dir_fp, model, processor, device)
         all_results[dir_fp] = dir_result
 
 def main():
     mp.set_start_method('spawn')
     
-    tracks_dir = '/mnt/opr/levlevi/player-re-id/src/testing/ocr_analysis/_50_game_reid_benchmark_/labeled-tracks'
-    out_fp = '/mnt/opr/levlevi/player-re-id/src/testing/ocr_analysis/results.json'
+    tracks_dir = '/mnt/opr/levlevi/player-re-id/src/data/_50_game_reid_benchmark_/labeled-tracks'
+    out_fp = '/mnt/opr/levlevi/player-re-id/src/data/florence_100_track_bm_results.json'
+    dirs = glob(os.path.join(tracks_dir, '*', '*'))
     dirs = glob(os.path.join(tracks_dir, '*', '*'))
     
     manager = mp.Manager()
