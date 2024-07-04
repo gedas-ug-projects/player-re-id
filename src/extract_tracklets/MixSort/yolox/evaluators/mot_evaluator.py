1a3567daadb44b79cb7f8046dd468e602d3ff95d
leharris3 <levlevi@email.unc.edu>
2024-07-04 15:57:36 -0400

    more notes (:

--- src/extract_tracklets/MixSort/yolox/evaluators/mot_evaluator.py
+++ src/extract_tracklets/MixSort/yolox/evaluators/mot_evaluator.py
@@ -1,18 +1,12 @@
-import contextlib
-import io
-import json
-import tempfile
 import torch
 import logging
 import numpy as np
 
 from yolox.utils import (
-    is_main_process,
     postprocess,
     xyxy2xywh,
 )
 from yolox.mixsort_tracker.mixsort_iou_tracker import MIXTracker
-from collections import defaultdict
 from loguru import logger
 from tqdm import tqdm
 
@@ -22,6 +16,9 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+# TODO: optimize
+    # maybe use a different library?
+    # find some way to do this in paralell?
 def write_results(filename, results):
     save_format = "{frame},{id},{x1},{y1},{w},{h},{s},-1,-1,-1\n"
     with open(filename, "w") as f:
@@ -41,27 +38,6 @@ def write_results(filename, results):
                 )
                 f.write(line)
     # logger.info('save results to {}'.format(filename))
-
-
-def write_results_no_score(filename, results):
-    save_format = "{frame},{id},{x1},{y1},{w},{h},-1,-1,-1,-1\n"
-    with open(filename, "w") as f:
-        for frame_id, tlwhs, track_ids in results:
-            for tlwh, track_id in zip(tlwhs, track_ids):
-                if track_id < 0:
-                    continue
-                x1, y1, w, h = tlwh
-                line = save_format.format(
-                    frame=frame_id,
-                    id=track_id,
-                    x1=round(x1, 1),
-                    y1=round(y1, 1),
-                    w=round(w, 1),
-                    h=round(h, 1),
-                )
-                f.write(line)
-    # logger.info('save results to {}'.format(filename))
-
 
 class MOTEvaluator:
     """
@@ -192,7 +168,14 @@ class MOTEvaluator:
 
                 # logger.debug(f"outputs post-proccessed shape: {outputs}")
 
-                # what is going on here?
+                # seems to be we are just calculating
+                # TODO: this code needs to modularized
+                    # 1. generate all batches from generator as a single iterable tensor
+                        # we should perform all expensive copy ops from CPU -> GPU in paralell up front
+                    # 2. perform inference on batches and append results (already on GPU) to a list
+                    # 3. post process all results
+                    # 4. calculate all results
+                    
                 if (cur_iter) % batch_size == 0:
                     
                     # output, image info, video id
@@ -205,21 +188,34 @@ class MOTEvaluator:
                         logger.debug(f"id: {id}")
 
                         # TODO: can most likely optimize this function
+                        # TODO: do this in paralell
                         output_results = self.convert_to_coco_format(o, ii, id)
 
                         logger.debug(f"output_results: {output_results}")
                         data_list.extend(output_results)
                         logger.debug(f"data_list: {data_list}")
 
+                        # what is at o[0]?
                         if o[0] is not None:
                             logger.debug(f"o[0]: {o[0]}")
+                            
+                            # what are these online targets?
+                            # TODO: copy all origin images to cuda as a single tensor
+                            # TODO: optimize `update` function
                             online_targets = tracker.update(
                                 o[0], ii, self.img_size, origin_imgs.squeeze(0).cuda()
                             )
                             logger.debug(f"online_targets: {online_targets}")
+                            
+                            # what is `online_tlwhs` var?
+                            # what is `t` temp var?
                             online_tlwhs, online_ids, online_scores = [], [], []
                             for t in online_targets:
+                                
+                                # what is tid, do i care?
                                 tlwh, tid, score = t.tlwh, t.track_id, t.score
+                                
+                                # silly hard coded threshold
                                 vertical = tlwh[2] / tlwh[3] > 1.6
                                 if (
                                     tlwh[2] * tlwh[3] > self.args.min_box_area
@@ -228,6 +224,7 @@ class MOTEvaluator:
                                     online_tlwhs.append(tlwh)
                                     online_ids.append(tid)
                                     online_scores.append(score)
+                                    
                             logger.debug(f"online_tlwhs: {online_tlwhs}")
                             logger.debug(f"online_ids: {online_ids}")
                             logger.debug(f"online_scores: {online_scores}")
@@ -244,16 +241,25 @@ class MOTEvaluator:
                             # why write results every forward pass?
 
                     # reset
+                    # TODO: come up w/ a better memory management system
+                    # TODO: benchmark: are larger forward passes actually faster than processing videos one at a time?
                     del imgs_batch
+                    
                     imgs_batch = torch.tensor([]).cuda()
                     frame_ids_batch = torch.tensor([])
                     video_ids_batch = torch.tensor([])
                     info_images_batch = []
                     ids_batch = []
 
+        # TODO: write results to file in paralell
+            # this function just iterates over results and writes each line to a file
         write_results(tracklets_out_path, results)
+        
+        # TODO: maybe give this function a meaningful return value?
         return None
 
+    
+    # TODO: some explicit typing / documentation of these variable names
     def convert_to_coco_format(self, outputs, info_imgs, ids):
         data_list = []
         for output, img_h, img_w, img_id in zip(
@@ -261,16 +267,26 @@ class MOTEvaluator:
         ):
             if output is None:
                 continue
+            
+            # expensive GPU -> CPU copy opp
+            # is this necessisary
+            # TODO: can we process a big tensor of results in paralell rather than iterating over results?
             output = output.cpu()
+            
             bboxes = output[:, 0:4]
+            
             # preprocessing: resize
             scale = min(
                 self.img_size[0] / float(img_h), self.img_size[1] / float(img_w)
             )
+            
+            # TODO: add explict types for bbxs
             bboxes /= scale
             bboxes = xyxy2xywh(bboxes)
             cls = output[:, 6]
             scores = output[:, 4] * output[:, 5]
+            
+            # TODO: maybe a faster way to do this?
             for ind in range(bboxes.shape[0]):
                 label = self.dataloader.dataset.class_ids[int(cls[ind])]
                 pred_data = {
@@ -281,4 +297,5 @@ class MOTEvaluator:
                     "segmentation": [],
                 }  # COCO json format
                 data_list.append(pred_data)
+                
         return data_list
